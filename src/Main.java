ed31a1cb8a2a74619aaffe48cb7d9bcf5a5be66a
Alex Kozloff <wec@tut.by>
2015-08-28 21:02:07 +0300

    Первая версия JavaFX программы

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
@@ -13,57 +11,24 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 
-    Stage window;
-    Scene scene1, scene2;
-
+    Button button;
 
     public static void main(String[] args) {
-
         launch(args);
-
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Главное окно");
 
-        window = primaryStage;
-
-        Label label1 = new Label("Добро пожаловать в первую сцену");
-        Button button1 = new Button("Идем в сцену 2");
-        button1.setOnAction(event -> window.setScene(scene2));
-
-        VBox layot1 = new VBox(20);
-        layot1.getChildren().addAll(label1, button1);
-        scene1 = new Scene(layot1,200,200);
-
-        Button button2 = new Button("Идем в сцену 1");
-        button2.setOnAction(event -> window.setScene(scene1));
-
-        StackPane layot2 = new StackPane();
-        layot2.getChildren().add(button2);
-        scene2 = new Scene(layot2, 600, 300);
-
-        window.setScene(scene1);
-        window.setTitle("Главное окно");
-        window.show();
-
-
-/*
-        primaryStage.setTitle("Заголовок моего первого окна");
         button = new Button();
-        button.setText("Click Me!");
-        button.setOnAction(event -> System.out.println("Прикольььнооооооо!!!"));
+        button.setText("Старт");
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
+        StackPane layot = new StackPane();
+        layot.getChildren().add(button);
 
-        Scene scene = new Scene(layout, 300, 250);
+        Scene scene = new Scene(layot, 300, 250);
         primaryStage.setScene(scene);
         primaryStage.show();
-*/
-
-
     }
-
-
 }
